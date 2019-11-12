dd9b6b95f0037fbf14ceaa313a77c2d223f2fea5
koichiohara <zm18431@josai.ac.jp>
2019-11-12 03:30:30 +0000

    11/12

--- ex6-5.js
+++ ex6-5.js
@@ -1,9 +1,9 @@
-var who='wolud'; function sayhello(){alert('Hello,'+who+'!')}
-function taro(){
-    who='太郎';
+var who65='wolud'; function sayhello65(){alert('Hello,'+who65+'!')}
+function taro65(){
+    who65='太郎';
 }
-function hanako(){
-    who='花子';
+function hanako65(){
+    who65='花子';
 }
 
 
