8899b8b30d9f0b7857d0ae9f672d2d868954c11c
koichiohara <zm18431@josai.ac.jp>
2020-01-07 03:25:42 +0000

    1/7

--- ex06-10.js
+++ ex06-10.js
@@ -1,4 +1,4 @@
-function hyoji(){
+function hyoji1(){
 //表示する文字
 var str="一文字ずつ表示します。";
 //テキストボックスの文字数
@@ -6,16 +6,30 @@ var cnt=document.timer.moji.value.length;
 //文字が全部表示されているか確認
 if(cnt<11){
 //現在より1文字多く切り出して表示
-document.timer.moji.value=str.substr(0,cnt+1);}
+document.timer.moji.value=str.substr1(0,cnt+1);}
 else{
 //全て表示されたら、空文字に戻す
 document.timer.moji.value="";}}
 function startfnc(){
 //関数hyoji()を1000ミリ秒間隔で呼び出す
-setInterval("hyoji()",1000);}
-function mes(){
+setInterval("hyoji1()",1000);}
+function mes1(){
 alert("３秒経ちました！");
 }
-function hyoji(){
-var str="./image/josai.png";
-document.timer./image/josai.png.value="./image/josai.jpg";}
+
+function hyoji2()
+{
+    var str="一文字ずつ表示します。";
+    var cnt=document.timer.moji.value.length;
+if(cnt<11)
+{
+    document.timer.moji.value=str.substr2(0,cnt+1);
+}
+else
+{
+    document.timer.moji.value="";
+}
+setTimeout("hyoji2()",1000);
+}
+
+function mes2(){document.getElementById('logo2').src='./image/josai.png';}
